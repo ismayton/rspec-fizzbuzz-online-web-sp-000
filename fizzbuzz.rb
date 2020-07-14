c1d9b57130319d82aa4b47d80b562206df99fbd5
Ian Mayton <ian.mayton@gmail.com>
2020-07-14 21:54:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,4 +3,9 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
     return "FizzBuzz"
-  elsif number % 3 == 0
+    elsif number % 3 == 0
+    return "Fizz"
+    elsif number % 5 == 0
+    return "Buzz"
+end
+end    
